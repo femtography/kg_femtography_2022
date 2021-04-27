ce66bf8014bd7d8491ad1de19c80c166cb7b210f
Jeffrey Soto <femtography@me.com>
2021-04-26 22:07:44 -0600

    commented my code so that translation to golang will be easier and issues should be limited syntax

--- safeplace.py
+++ safeplace.py
@@ -1,6 +1,9 @@
 import sys
 
+# Main function that takes in arguements from command line and returns phonetic conversion.
 def main(argv):
+    # Creating three variables. A string for stdout. A counter to trigger a stop in th while loop.
+    # As well as the hashmap to convert numerical values to their phonetic counterpart.
     final_print = "";
     counter = 0;
     dict = {
@@ -16,16 +19,20 @@ def main(argv):
         "9" : "Nine"
     }
 
+    # While loop to go through arguements provided from command line. It will use hash table to convert each value
+    # provided into the spelling of the number and then add it to final_print string.
     while counter < len(argv):
         arg = argv[counter];
         for val in arg:
             final_print = final_print + dict[val];
 
+        # Adds comma after each value in args but only if it is not the last item in list.
         if counter != len(argv) - 1:
             final_print = final_print + ",";
 
         counter += 1;
 
+    # Returning expected value.
     print(final_print)
 
 if __name__ == "__main__":
