4190587a6a633688285af761890972a3ddfc9c94
Jeffrey Soto <femtography@me.com>
2021-04-26 22:27:24 -0600

    translated while loop from python code, was really different in that golang does not have while loops and they have a variable type of rune which really set me back but was eventually overcome

--- main.go
+++ main.go
@@ -21,5 +21,19 @@ func main() {
     "8" : "Eight",
     "9" : "Nine",
   }
-  fmt.Println(args)
+
+  for counter < len(args) {
+    item := []rune(args[counter])
+
+    for i := 0; i < len(item); i++ {
+      r_val = r_val + hashy[string(item[i])]
+    }
+
+    if counter != len(args) - 1 {
+      r_val = r_val + ","
+    }
+
+    counter ++;
+  }
+  fmt.Println(r_val)
 }
