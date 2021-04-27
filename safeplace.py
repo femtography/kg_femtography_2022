6ad39281c815e6ec399680261f94e82e545f9960
Jeffrey Soto <femtography@me.com>
2021-04-26 21:55:10 -0600

    added three variables needed for script which were and empty string, pointer, and a dictionairy of base 10 numerical values and their phonetic counterpart

--- safeplace.py
+++ safeplace.py
@@ -1,7 +1,20 @@
 import sys
 
 def main(argv):
-
+    final_print = "";
+    counter = 0;
+    dict = {
+        "0" : "Zero",
+        "1" : "One",
+        "2" : "Two",
+        "3" : "Three",
+        "4" : "Four",
+        "5" : "Five",
+        "6" : "Six",
+        "7" : "Seven",
+        "8" : "Eight",
+        "9" : "Nine"
+    }
 
 if __name__ == "__main__":
    main(sys.argv[1:])
